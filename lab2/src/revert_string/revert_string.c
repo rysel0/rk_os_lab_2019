f3b8e43721e1e12d8ff53b6b162a3b6af75b60a5
rysel0 <khayrulin04@gmail.com>
2024-10-28 19:57:03 +0000

    реализован метод RevertString

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "revert_string.h"
 
 void RevertString(char *str)
 {
-	// your code here
-}
+    int first = 0;
+    int last = strlen(str) - 1;
+    char temp;
 
+    while (first < last) {
+      
+        temp = str[first];
+        str[first] = str[last];
+        str[last] = temp;
+
+        first++;
+        last--;
+    }
+}
+// void RevertString(char *str)
+// {
+//     char* temp = malloc(sizeof(char) * (strlen(str)));
+    
+//     for (int i = strlen(str) - 1; i >= 0; i--) {
+//         temp[strlen(str) - 1 - i] = str[i];
+//     }
+//     strcpy(str, temp);
+// }
